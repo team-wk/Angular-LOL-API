362cad4782d47eb3a7c03effa054298867115173
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2017-10-23 15:58:42 +0200

    prepare champions management

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AppService } from './app.service';
 import { FormsModule } from '@angular/forms';
 import { Champion } from './champion';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Component({
   selector: 'app-root',
@@ -29,5 +31,21 @@ export class AppComponent {
 
   deleteChampion(id: string) {
     this.appService.deleteChampion(id);
+  }
+
+  fillChampionsFromJson() {
+    this.appService.getChampionsJson().then((championsFromJson) => {
+      championsFromJson.forEach(champion => {
+          this.appService.setChampion(champion);
+      });
+    });
+  }
+
+  deleteAllChampions() {
+    this.appService.getChampions().then((champions) => {
+      champions.forEach(champion => {
+          this.appService.deleteChampion(champion._id);
+      });
+    });
   }
 }
--- front-end/src/app/app.service.ts
+++ front-end/src/app/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
 
 import { Champion } from './champion';
 
@@ -9,19 +10,25 @@ export class AppService {
   private url = 'http://localhost:4300/champions';
 
   constructor(private http: HttpClient) {
-  }
+  };
 
   getChampions(): Promise<Champion[]> {
     return this.http.get(this.url).toPromise().then((response) => {
       return response;
     });
-  }
+  };
 
   setChampion(data: Champion): Promise<any> {
-    return this.http.post(this.url, data).toPromise()
-  }
+    return this.http.post(this.url, data).toPromise();
+  };
 
   deleteChampion(id: string): Promise<any> {
-    return this.http.delete(this.url + '/' + id).toPromise()
-  }
+    return this.http.delete(this.url + '/' + id).toPromise();
+  };
+
+  getChampionsJson(): Promise<Champion[]> {
+    return this.http.get("../assets/data/champions.json").toPromise().then((response) => {      
+      return response;
+    });
+  };
 }
