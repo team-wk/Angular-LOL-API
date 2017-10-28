f6dea05f9d1a373aa803355175d24455a3937f25
Łukasz Woźniak <wozniak.lukasz.g@gmail.com>
2017-10-28 14:54:32 +0200

    mat design implementation

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { MatButtonModule,
+  MatCardModule,
+  MatProgressBarModule,
+  MatGridListModule,
+  MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
@@ -12,6 +18,12 @@ import { AppService } from './app.service';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatGridListModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
   ],
   providers: [
     AppService,
